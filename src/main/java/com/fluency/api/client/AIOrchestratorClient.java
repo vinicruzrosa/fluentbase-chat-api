f2f9148cbc0b97fc23c100514f51d5431073bc55
Vinicius Cruz <vini.cruz.roo@gmail.com>
2026-03-18 09:05:39 -0400

    "second-commit"

--- src/main/java/com/fluency/api/client/AIOrchestratorClient.java
+++
@@ -1,9 +0,0 @@
-package com.fluency.api.client;
-
-import com.fluency.api.model.ChatAnalysis;
-import reactor.core.publisher.Mono;
-
-public interface AIOrchestratorClient {
-
-    Mono<ChatAnalysis> analyzeMessage(String text);
-}
--- src/main/java/com/fluency/api/dto/ChatMessageRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.fluency.api.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ChatMessageRequestDto(
-        @NotBlank(message = "A mensagem não pode ser vazia")
-        String message
-) {}
